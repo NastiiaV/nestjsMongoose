073ccd71745ec5b123104684fa82236263a24fd3
NastiiaV <awerga13@gmail.com>
2021-04-21 11:02:44 +0300

    db heroku

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,14 @@ import { HealthModule } from './health/health.module';
 import { TerminusModule } from '@nestjs/terminus';
 import { ConfigModule } from '@nestjs/config';
 
+// USER = admin
+// PASSWORD = admin123
+// DB = nestRoute
+//`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ukfb4.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
 @Module({
   imports: [RoutecModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ukfb4.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`,
+    MongooseModule.forRoot( process.env.MONGO_DB_URI || 'mongodb+srv://admin:admin123@cluster0.ukfb4.mongodb.net/nestRoute?retryWrites=true&w=majority',
       { useNewUrlParser: true }),
     HealthModule, TerminusModule],
   controllers: [AppController, HealthController],
